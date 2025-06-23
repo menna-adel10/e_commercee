ea572a32a310c71af57151242e8824a3b0b659c2
menna-adel10 <85319557+menna-adel10@users.noreply.github.com>
2025-06-23 14:32:47 +0300

    initialize supabase in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:e_commercee/views/nav_bar/ui/main_home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/app_colors.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://nsltfnyiwdiurmquqeol.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zbHRmbnlpd2RpdXJtcXVxZW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2NzY4NzQsImV4cCI6MjA2NjI1Mjg3NH0.hcd3FMFI8A8Al2rG7EXZdqzYDFBxwxJglFthDeOE6cE',
+  );
   runApp(const MyApp());
 }
 
